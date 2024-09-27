e891acae9463b502548e7411f0ea3c48025f906e
MstyslavSoroka <134134015+MstyslavSoroka@users.noreply.github.com>
2024-09-27 18:21:20 +0000

    Deploying to gh-pages from @ MstyslavSoroka/Liblary@5d12aecf9190268d09f71e10d4088ac384f98ff8 🚀

---
+++ index.188934c9.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("viewPopularBooks"),t=document.getElementById("viewActiveUsers"),n=document.getElementById("statsBooksContainer"),s=document.getElementById("statsUsersContainer");e.addEventListener("click",(function(){n.style.display="flex",s.style.display="none"})),t.addEventListener("click",(function(){s.style.display="flex",n.style.display="none"}));
+//# sourceMappingURL=index.188934c9.js.map
